b94db6019600d2df8001a033769a37ef9e6d85a0
evilsinnes <pilobodzinsky@yandex.com>
2025-01-04 15:57:50 +0300

    compliteTask

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Павел!");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName);
+
+
+
+
     }
 }
